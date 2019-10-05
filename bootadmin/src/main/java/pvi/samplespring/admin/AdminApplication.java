e115c6c6a8cb3a72399039aa3bbd08764f494c1d
Petr Igrevskiy <pigrevskiy@reputation.com>
2019-10-04 21:52:12 -0700

    Added changes for kuberenetes discovery

--- bootadmin/src/main/java/pvi/samplespring/admin/AdminApplication.java
+++ bootadmin/src/main/java/pvi/samplespring/admin/AdminApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
+@EnableScheduling
+@Configuration
 public class AdminApplication {
 
 	public static void main(String[] args) {
--- configserver/src/main/java/pvi/samplespring/admin/ConfigServerApplication.java
+++ configserver/src/main/java/pvi/samplespring/admin/ConfigServerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
-@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+@Configuration
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
